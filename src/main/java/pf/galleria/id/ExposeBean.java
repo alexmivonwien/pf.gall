03998d2d49b8cdcb743e376bf24d74bba1924b75
alexmivonwien <alexmivonwien@gmail.com>
2019-09-12 06:18:51 +0200

    first commit

---
+++ src/main/java/pf/galleria/id/ExposeBean.java
@@ -0,0 +1,36 @@
+package pf.galleria.id;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Named;
+
+/**
+* @author alex-mi
+* 
+* We store images on the file system with paths to them stored in the database
+* 
+* @see https://stackoverflow.com/questions/3748/storing-images-in-db-yea-or-nay
+* @see https://habiletechnologies.com/blog/better-saving-files-database-file-system/
+* 
+* @see http://balusc.omnifaces.org/2011/09/communication-in-jsf-20.html#ProcessingGETRequestParameters
+*
+*/
+@Named
+@javax.faces.view.ViewScoped
+public class ExposeBean implements Serializable {
+
+	private List<String> images = new ArrayList<>();
+	
+	
+	{
+   	 images.add("600_5308AA4BB69142E3AB05DF9BA394A133.jpg");
+	 images.add("600_AF3E4D6017BB4C6C8BA953D356FBE000.jpg");
+	 images.add("600_BC152BACEF144A75AAE8785422BADEFF.jpg");
+   }
+
+	public List<String> getImages() {
+       return images;
+   }
+}
